4f713b72abbf860b4ce38b2ba3a64cbe06e62fcc
Rhea Rivera <rjuner@gmail.com>
2016-07-23 18:27:59 -0500

    api routes math working

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,10 +1,22 @@
 var friendArray = [
 	{
-		name: 'lita',
+		name: 'Lita',
 		photo: 'http://vignette3.wikia.nocookie.net/sailormoon/images/4/4e/Lita.jpg/revision/latest?cb=20120318021926',
 		scores: 
 		[4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
-	}
+	}, 
+		{
+		name: 'Rei',
+		photo: 'https://66.media.tumblr.com/36226128300c506f4431ba73e5f171e4/tumblr_nvryw0tEYj1ugye8so1_250.png',
+		scores: 
+		[3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+	}, 
+		{
+		name: 'Minako',
+		photo: 'http://vignette3.wikia.nocookie.net/sailormoon/images/4/4c/Minako_aino_anime_2.png/revision/latest?cb=20121205025554e.net/sailormoon/images/4/4e/Lita.jpg/revision/latest?cb=20120318021926',
+		scores: 
+		[2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+	}, 
 ];
 
 module.exports = friendArray;
--- app/routing/api-routes.js
+++ app/routing/api-routes.js
@@ -11,9 +11,45 @@ var path 		= require('path');
 
 module.exports = function(app){
 
+	//GET REQUESTS
+
 	//when users visit 'api/friends/' they should get the JSON data
 	app.get('/api/friends', function(req, res){
 		res.json(friendData);
 	});
-	
+
+	//API POST REQUESTS
+	app.post('/api/friends', function(req, res){
+
+		//do matching math...
+
+		var newFriend = req.body.scores;
+
+		for(var i = 0; i < friendData.length; i++){
+
+			var friendScore = (friendData[i].scores);
+			console.log("friend " + i + "'s array: ");
+
+			var totalDifference = 0
+
+			for(var k = 0; k < newFriend.length; k++){
+
+				var diff = Math.abs(friendScore[k] - newFriend[k]);
+
+				totalDifference += diff; 
+
+				//console.log(diff);
+
+			}
+
+			console.log(totalDifference);
+
+
+		}
+
+		//this will send the BEST FRIEND MATCH to ajax .post ...function(DATA)
+		res.json(friendData[0]);
+
+	});
+
 }
