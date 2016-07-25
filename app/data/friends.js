2c316bd13bddf040d023c0473464e164ceb9c176
Rhea Rivera <rjuner@gmail.com>
2016-07-25 00:16:56 -0500

    survey should be working now

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,22 +1,34 @@
 var friendArray = [
 	{
 		name: 'Lita',
-		photo: 'http://vignette3.wikia.nocookie.net/sailormoon/images/4/4e/Lita.jpg/revision/latest?cb=20120318021926',
+		photo: 'https://66.media.tumblr.com/18567d7e9de7a21266dba3fea041f012/tumblr_o64tphPUp21uwhtt4o1_500.gif',
 		scores: 
-		[4, 4, 4, 4, 4, 4, 4, 4, 4, 4]
+		[5, 4, 2, 2, 3, 5, 4, 2, 4, 5]
 	}, 
 		{
 		name: 'Rei',
-		photo: 'https://66.media.tumblr.com/36226128300c506f4431ba73e5f171e4/tumblr_nvryw0tEYj1ugye8so1_250.png',
+		photo: 'https://66.media.tumblr.com/43b0af965f8edba99d7f34b38c21bce6/tumblr_o7vfjnz5UP1rzlojlo1_500.gif',
 		scores: 
-		[3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+		[3, 2, 4, 4, 2, 3, 2, 1, 1, 4]
 	}, 
 		{
-		name: 'Minako',
-		photo: 'http://vignette3.wikia.nocookie.net/sailormoon/images/4/4c/Minako_aino_anime_2.png/revision/latest?cb=20121205025554e.net/sailormoon/images/4/4e/Lita.jpg/revision/latest?cb=20120318021926',
+		name: 'Mina',
+		photo: 'http://media.tumblr.com/29a528d8257cd557bc4e0db8c9a6a0c2/tumblr_inline_mnfi03d8RU1qiw26m.gif',
 		scores: 
-		[2, 2, 2, 2, 2, 2, 2, 2, 2, 2]
+		[2, 3, 3, 3, 4, 4, 3, 4, 2, 3]
 	}, 
+		{
+		name: 'Serena',
+		photo: 'https://66.media.tumblr.com/ae031ca13f83f314a91fd3b71724ac1f/tumblr_nmlouqRteC1r46ugeo1_540.gif',
+		scores: 
+		[1, 5, 1, 5, 5, 1, 5, 5, 5, 1]
+	}, 
+		{
+		name: 'Amy',
+		photo: 'https://38.media.tumblr.com/6c18cb004e25de8762482eb6542659c4/tumblr_ntaonhov0i1sz111so1_540.gif',
+		scores: 
+		[4, 1, 5, 1, 1, 2, 1, 3, 3, 2]
+	}
 ];
 
 module.exports = friendArray;
